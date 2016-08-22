8ae99e4cef37432a7d2b9adaefa3057e34ebf456
Carl <crucialcarl@gmail.com>
2016-08-21 21:00:50 -0400

    update

--- game_functions.py
+++ game_functions.py
@@ -3,7 +3,7 @@ import pygame
 
 def check_events():
   """ Respond to keypresses and mouse events """
-  for event inpygame.event.get():
+  for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
 
