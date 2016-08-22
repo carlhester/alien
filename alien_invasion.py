e62468d0abcec51d181637abd024451e909a185c
Carl <crucialcarl@gmail.com>
2016-08-21 20:49:44 -0400

    use settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,16 @@
 import sys
 import pygame
 
+from settings import Settings
+
+
 def run_game():
   pygame.init()
-  screen = pygame.display.set_mode((1200, 800))
+  ai_settings = Settings()
+  screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption(("Alien Invasion"))
+
+
 
   # start the main loop
   while True:
@@ -13,6 +19,10 @@ def run_game():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
+
+
+    # Redraw the screen during each pass  
+    screen.fill(ai_settings.bg_color)
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
