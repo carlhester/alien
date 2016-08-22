2659bf96c24f7a2821c17bc312a411e9cd304fb3
Carl <crucialcarl@gmail.com>
2016-08-21 21:47:27 -0400

    rect to center

--- ship.py
+++ ship.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Ship():
-  def __init__(self, screen):
+  def __init__(self, ai_settings, screen):
     """ Initialize the ship and set its starting position"""
     self.screen = screen
+    self.ai_settings = ai_settings
 
     # Load the shp image and get its rect
     self.image = pygame.image.load('images/ship.bmp')
@@ -11,19 +12,22 @@ class Ship():
     self.screen_rect = screen.get_rect()
 
     # start each new ship at the bottom center
-    self.rect.centerx = self.screen_rect.centerx
-    self.rect.bottom = self.screen_rect.bottom
-
+    self.center = float(self.rect.centerx)
+  
     # movement flag
     self.moving_right = False
     self.moving_left = False
 
   def update(self):
     """ update the ships position based on the movement flag """
+    # update the ships center value, not the rect
     if self.moving_right: 
-      self.rect.centerx += 1
+      self.center += self.ai_settings.ship_speed_factor
     elif self.moving_left:
-      self.rect.centerx -= 1
+      self.center -= self.ai_settings.ship_speed_factor
+
+    # update the rect object from self.center
+    self.rect.centerx = self.center
 
 
   def blitme(self):
