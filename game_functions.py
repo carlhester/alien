7c6eb8491f9618d80aa6a89ce7917ce267e2a842
Carl <crucialcarl@gmail.com>
2016-08-21 21:39:54 -0400

    next

--- game_functions.py
+++ game_functions.py
@@ -1,11 +1,19 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
   """ Respond to keypresses and mouse events """
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_RIGHT:
+        # Move ship to the right
+        ship.rect.centerx += 1
+      elif event.key == pygame.K_LEFT:
+        # Move the ship left
+        ship.rect.centerx -= 1
+      
 
 def update_screen(ai_settings, screen, ship):
   """ Update images on the screen anf lip to new screen """
